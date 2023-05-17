e096afb6c2e661849fb5421c33ddf884d0f41d0c
CY-Fafix <ProtonPingouin@protonmail.com>
2023-05-17 16:02:09 +0200

    Création des classes User, Etudiant, Admin, gestionnaire

--- src/classes/User.php
+++ src/classes/User.php
@@ -6,13 +6,18 @@ class Utilisateur {
   protected $prenom;
   protected $email;
   protected $motDePasse;
+  protected $telephone;
+  protected $ville;
+  //Rajouter un role;
 
   // Constructeur
-  public function __construct($nom, $prenom, $email, $motDePasse) {
+  public function __construct($nom, $prenom, $email, $motDePasse, $telephone, $ville) {
     $this->nom = $nom;
     $this->prenom = $prenom;
     $this->email = $email;
     $this->motDePasse = $motDePasse;
+    $this->telephone = $telephone;
+    $this->ville = $ville;
   }
 
   // Getters
@@ -30,6 +35,14 @@ class Utilisateur {
 
   public function getMotDePasse() {
     return $this->motDePasse;
+  }
+
+  public function getTelephone(){
+    return $this->telephone;
+  }
+
+  public function getVille(){
+    return $this->ville;
   }
 
   // Setters
@@ -52,6 +65,14 @@ class Utilisateur {
   public function setMotDePasse($motDePasse) {
     // Assurez-vous de mettre en œuvre une certaine logique de hachage sécurisée pour le mot de passe
     $this->motDePasse = password_hash($motDePasse, PASSWORD_DEFAULT);
+  }
+
+  public function setTelephone($telephone){
+    $this->telephone = $telephone;
+  }
+
+  public function setVille($ville){
+    $this->ville = $ville;
   }
 }
 
