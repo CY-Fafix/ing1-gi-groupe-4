666d28382ee54a7b785af8784b9eb5ef43c4aee5
Ananaee <viramaanae@cy-tech.fr>
2023-05-23 15:49:17 +0200

    Amélioration de l'apparence de l'accueil

--- public/index.php
+++ public/index.php
@@ -21,97 +21,179 @@
 	<div class="body">
 		<div class="parag1">
 			
-			<a href="" class=""> <B>Data Challenge</B></a>
+			<a href="" class=""> <B>Data Challenge 1</B></a>
 			<p>Lorem ipsum kdpoefjpzok</p>
 			
 			<div class="projet_data">
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-					<p>Projet data</p>
-				</a>
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-					<p>Projet data</p>
-				</a>
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-					<p>Projet data</p>
-				</a>
+				<ul class="sujet">
+					<li>
+						<a href="">
+						<article class="card">
+							<div class="temporary_text">
+								<!-- <img src="assets/truc.jpg" class="" alt="bonbon miel"> -->
+							</div>
+							<div class="card_content">
+								<span class="card_title">Projet data 1</span>
+								<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+								<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+								
+							</div>
+						</article>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<article class="card">
+								<div class="temporary_text">
+									<!-- <img src="assets/truc.jpg" class="challenge" alt="bonbon miel"> -->
+								</div>
+								<div class="card_content">
+									<span class="card_title">Projet data 2</span>
+									<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+									<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+									
+								</div>
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<article class="card">
+								<div class="temporary_text">
+									<!-- <img src="assets/truc.jpg" class="challenge" alt="bonbon miel"> -->
+								</div>
+								<div class="card_content">
+									<span class="card_title">Projet data 3</span>
+									<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+									<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+									
+								</div>
+							</article>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 		
 		<div class="parag1">
 			
-			<a href="" class=""> <B>Data Challenge</B></a>
+			<a href="" class=""> <B>Data Challenge 2</B></a>
 			<p>Lorem ipsum2 kdpoefjpzok</p>
 			
 			<div class="projet_data">
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-				</a>
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-				</a>
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-				</a>
+			<ul class="sujet">
+					<li>
+						<a href="">
+						<article class="card">
+							<div class="temporary_text">
+								<!-- <img src="assets/truc.jpg" class="" alt="bonbon miel"> -->
+							</div>
+							<div class="card_content">
+								<span class="card_title">Projet data 1</span>
+								<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+								<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+								
+							</div>
+						</article>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<article class="card">
+								<div class="temporary_text">
+									<!-- <img src="assets/truc.jpg" class="challenge" alt="bonbon miel"> -->
+								</div>
+								<div class="card_content">
+									<span class="card_title">Projet data 2</span>
+									<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+									<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+									
+								</div>
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<article class="card">
+								<div class="temporary_text">
+									<!-- <img src="assets/truc.jpg" class="challenge" alt="bonbon miel"> -->
+								</div>
+								<div class="card_content">
+									<span class="card_title">Projet data 3</span>
+									<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+									<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+									
+								</div>
+							</article>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 		
 		<div class="parag1">
 			
-			<a href="" class=""> <B>Data Challenge</B></a>
+			<a href="" class=""> <B>Data Challenge 3</B></a>
 			<p>Lorem ipsum kdpoefjpzok</p>
 			
 			<div class="projet_data">
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-				</a>
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-				</a>
-				<a href="">
-					<img src="assets/truc.jpg" class="challenge" alt="bonbon miel">
-				</a>
+			<ul class="sujet">
+					<li>
+						<a href="">
+						<article class="card">
+							<div class="temporary_text">
+								<!-- <img src="assets/truc.jpg" class="" alt="bonbon miel"> -->
+							</div>
+							<div class="card_content">
+								<span class="card_title">Projet data 1</span>
+								<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+								<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+								
+							</div>
+						</article>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<article class="card">
+								<div class="temporary_text">
+									<!-- <img src="assets/truc.jpg" class="challenge" alt="bonbon miel"> -->
+								</div>
+								<div class="card_content">
+									<span class="card_title">Projet data 2</span>
+									<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+									<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+									
+								</div>
+							</article>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<article class="card">
+								<div class="temporary_text">
+									<!-- <img src="assets/truc.jpg" class="challenge" alt="bonbon miel"> -->
+								</div>
+								<div class="card_content">
+									<span class="card_title">Projet data 3</span>
+									<span class="card_subtitle">This is a subtitle of this page. Let us see how it looks on the Web.</span>
+									<p class="card_description">Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.</p>
+									
+								</div>
+							</article>
+						</a>
+					</li>
+				</ul>
 			</div>
 		</div>
 		<br>
 		
 	
 
-        <!-- Button to open the modal login form -->
-        <button onclick="document.getElementById('id01').style.display='block'" class="loginbtn">Login</button>
+        
 
-        <!-- The Modal -->
-        <div id="id01" class="modal">
-        <span onclick="document.getElementById('id01').style.display='none'"
-        class="close" title="Close Modal">&times;</span>
+       
 
-        <!-- Modal Content -->
-        <form class="modal-content animate" action="/action_page.php">
-            <div class="imgcontainer">
-                <img src="assets/truc.jpg" alt="Avatar" class="avatar">
-            </div>
-
-            <div class="container">
-                <label for="uname" class="label"><b>Username</b></label>
-                <input type="text" class="input" placeholder="Enter Username" name="uname" required>
-
-                <label for="psw"class="label"><b>Password</b></label>
-                <input type="password" class="input" placeholder="Enter Password" name="psw" required>
-
-                <button type="submit" class="loginbtn2">Login</button>
-                <label class="label">
-                    <input type="checkbox" checked="checked" class="input" name="remember"> Remember me
-                </label>
-            </div>
-
-            <div class="containerBas" style="background-color:#f1f1f1">
-                <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-                <!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
-            </div>
-        </form>
-    </div>
 
 
 </body>
--- src/views/header.php
+++ src/views/header.php
@@ -16,15 +16,19 @@ session_start();
 			<li><a href="Acceuil.html" class="active">Accueil</a></li>
 			<li><a href="Challenges.html">Challenges</a></li>
 			<li><a href="Contacts.html">Contacts</a></li>
-			<li><a href="Connexion.html" class="split">
-					<?php
-					if(isset($_SESSION['utilisateur_connecte'])){
-						echo('Déconnexion');
-					} else {
-						echo('Connexion');
-					}
-				?> 
-			</a></li>
+			<li>
+				<!-- Button to open the modal login form -->
+        		<button onclick="document.getElementById('id01').style.display='block'" id="loginbtn" class="split">Connexion</button>
+			</li>
+			<!-- <li><a href="Connexion.html" class="split"> -->
+					<!-- <?php
+					// if(isset($_SESSION['utilisateur_connecte'])){
+					// 	echo('Déconnexion');
+					// } else {
+					// 	echo('Connexion');
+					// }
+				?>  -->
+			<!-- </a></li> -->
 			
 
 
@@ -32,4 +36,35 @@ session_start();
 	</nav>
 
 </header>
+
+<!-- The Modal -->
+<div id="id01" class="modal">
+			<span onclick="document.getElementById('id01').style.display='none'"
+			class="close" title="Close Modal">&times;</span>
+
+			<!-- Modal Content -->
+			<form class="modal-content animate" action="/action_page.php">
+				<div class="imgcontainer">
+					<img src="assets/truc.jpg" alt="Avatar" class="avatar">
+				</div>
+
+				<div class="container">
+					<label for="uname" class="label"><b>Nom d'utilisateur</b></label>
+					<input type="text" class="input" placeholder="Enter Username" name="uname" required>
+
+					<label for="psw"class="label"><b>Mot de passe</b></label>
+					<input type="password" class="input" placeholder="Enter Password" name="psw" required>
+
+					<button type="submit" class="loginbtn2">Connexion</button>
+					<label class="label">
+						<input type="checkbox" checked="checked" class="input" name="remember"> Se rappeler de moi
+					</label>
+				</div>
+
+				<div class="containerBas" style="background-color:#f1f1f1">
+					<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Annuler</button>
+					<!-- <span class="psw">Forgot <a href="#">password?</a></span> -->
+				</div>
+			</form>
+    	</div>
 
