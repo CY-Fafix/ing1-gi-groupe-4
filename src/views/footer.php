5b460de2625fa7ec70974f410cff569b6ca8efb8
Ananaee <viramaanae@cy-tech.fr>
2023-05-24 11:46:10 +0200

    Changer l'apparence du site et ajouter un footer

---
+++ src/views/footer.php
@@ -0,0 +1,23 @@
+
+<!-- Add icon library -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<footer>
+    <div class="bas">
+        <div class="message">
+            <p>
+            Lorem ipsum dolor, sit amet  expedita exercitationem recusandae aut dolor tempora aperiam itaque possimus at, cupiditate earum, quae repudiandae aspernatur? Labore minus soluta consequatur placeat.
+            </p>
+
+        </div>
+        <div class="social">
+            <!-- Add font awesome icons -->
+            <a href="#" class="fa fa-facebook"></a>
+            <a href="#" class="fa fa-twitter"></a>
+            <a href="#" class="fa fa-instagram"></a>
+        </div>
+        <div class="copyrights">
+            <p>© 2023 Projet_GI 
+        </div>
+
+</footer>
