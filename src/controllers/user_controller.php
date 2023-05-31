086da80ab3396ba884e00e5633a16d6b2a4f3561
AdrienJcb <jacobadri@cy-tech.fr>
2023-05-31 15:54:49 +0200

    ajout backend d du formulaire de contact

--- src/controllers/user_controller.php
+++ src/controllers/user_controller.php
@@ -327,6 +327,21 @@ class UserController {
         }
         return $analyses;
     }
+    public function contact($email, $objet, $contenu){
+        try {
+            $email_admin = "projeting1pafa@gmail.com";
+            $contenu .= "\r\nFrom: " . $email;
+            $mailSent = mail($email_admin, $objet, $contenu);
+            if ($mailSent) {
+                echo 'Envoyé!';
+            } else {
+                echo 'Échec de l\'envoi de l\'e-mail.';
+            }
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
     
 }
+    
 ?>
